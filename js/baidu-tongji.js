12be84ea380b35277be6329e725adb82516104f5
gzttcydxx <1023645044@qq.com>
2021-11-15 16:57:50 +0800

    Deployed 7ee7dcf with MkDocs version: 1.2.3

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a2aa967a1e21a799e8f5e1a0ffd731f1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
